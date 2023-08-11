79018771567e3b4f3fc9ec9630ff202dfdb10652
Максим <novokshonov_m@mail.ru>
2023-08-11 16:19:55 +0300

    Update: valueChanges subscription added

--- src/app/shared/conteniers/header/header.component.ts
+++ src/app/shared/conteniers/header/header.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   searchForm: FormGroup;
+  private searchSubscription$: Subscription = new Subscription();
+
   constructor(private formBuilder: FormBuilder) {
     this.searchForm = this.formBuilder.group({
       search: [''],
     });
   }
 
-  submit() {}
+  get searchString(): FormControl {
+    return this.searchForm.get('search') as FormControl;
+  }
+
+  ngOnInit(): void {
+    this.searchSubscription$.add(
+      this.searchForm.valueChanges
+        .pipe(distinctUntilChanged(), debounceTime(300))
+        .subscribe(() => console.log('Search string = ' + this.searchString.value)),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.searchSubscription$.unsubscribe();
+  }
 }
